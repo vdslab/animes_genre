cb7b6105970690fe8d1240808fa0b2b6d11ea1f3
H153165g <chrn22018@g.nihon-u.ac.jp>
2024-12-09 21:56:19 +0900

    ミニマップ完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
+import "./app.css";
 
 function App() {
   const [pagestatus, setStatus] = useState(false);
   const [nodedata, setNodedata] = useState([]);
   const [scales, setScales] = useState({});
   const svgRef = useRef(null); // SVG を参照するための useRef
-  const [zoomscale, setZoomscale] = useState({});
+  const [zoomscale, setZoomscale] = useState({ k: 1, x: 0, y: 0 });
 
   useEffect(() => {
     // データの読み込み
@@ -42,14 +43,8 @@ function App() {
       }
     });
 
-    scale["xScale"] = d3
-      .scaleLinear()
-      .domain([xmin, xmax])
-      .range([10, 1000 + 5]);
-    scale["yScale"] = d3
-      .scaleLinear()
-      .domain([ymin, ymax])
-      .range([10, 1000 + 5]);
+    scale["xScale"] = d3.scaleLinear().domain([xmin, xmax]).range([0, 1000]);
+    scale["yScale"] = d3.scaleLinear().domain([ymin, ymax]).range([0, 1000]);
 
     return scale;
   };
@@ -77,72 +72,85 @@ function App() {
   }, [pagestatus]); // pagestatus が true になったときにズームを設定
 
   return pagestatus ? (
-    <div>
-      <svg
-        ref={svgRef}
-        width="1000"
-        height="1000"
-        style={{ top: 20, right: 30, bottom: 30, left: 40 }}
-      >
-        <g>
-          {/* ノードを描画 */}
-          {nodedata.map((node, index) => {
-            if (zoomscale.k < 3) {
-              return (
-                <ellipse
-                  key={index}
-                  cx={scales.xScale(node.x)}
-                  cy={scales.yScale(node.y)}
-                  rx="10"
-                  ry="10"
-                  fill={node.color}
-                  onClick={() => {
-                    console.log(node.animename);
-                  }}
-                  style={{ cursor: "pointer" }}
-                ></ellipse>
-              );
-            } else {
-              return (
-                <image
-                  key={index}
-                  x={scales.xScale(node.x) - 5} // イメージを中央に配置
-                  y={scales.yScale(node.y) - 5} // イメージを中央に配置
-                  width="10" // サイズ調整
-                  height="10" // サイズ調整
-                  href={node.coverImage}
-                  onClick={() => {
-                    console.log(node.animename);
-                  }}
-                  style={{ cursor: "pointer" }}
-                />
-              );
-            }
-          })}
-        </g>
-      </svg>
-      <div>
-        <svg width="500" height="500">
+    <div className="container">
+      <div className="graph">
+        <svg
+          ref={svgRef}
+          width="1000"
+          height="1000"
+          style={{ top: 20, right: 30, bottom: 30, left: 40 }}
+        >
           <g>
             {/* ノードを描画 */}
             {nodedata.map((node, index) => {
-              return (
-                <ellipse
-                  key={index}
-                  cx={scales.xScale(node.x) / 2}
-                  cy={scales.yScale(node.y) / 2}
-                  rx="5"
-                  ry="5"
-                  fill={node.color}
-                  onClick={() => {
-                    console.log(node.animename);
-                  }}
-                  style={{ cursor: "pointer" }}
-                ></ellipse>
-              );
+              if (zoomscale.k < 3) {
+                return (
+                  <ellipse
+                    key={index}
+                    cx={scales.xScale(node.x)}
+                    cy={scales.yScale(node.y)}
+                    rx="10"
+                    ry="10"
+                    fill={node.color}
+                    onClick={() => {
+                      console.log(node.animename);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  ></ellipse>
+                );
+              } else {
+                return (
+                  <image
+                    key={index}
+                    x={scales.xScale(node.x) - 5} // イメージを中央に配置
+                    y={scales.yScale(node.y) - 5} // イメージを中央に配置
+                    width="10" // サイズ調整
+                    height="10" // サイズ調整
+                    href={node.coverImage}
+                    onClick={() => {
+                      console.log(node.animename);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  />
+                );
+              }
             })}
           </g>
         </svg>
+      </div>
+      <div>
+        <div className="graph">
+          <svg width="500" height="500">
+            <g>
+              <rect
+                x={-zoomscale.x / 2 / zoomscale.k}
+                y={-zoomscale.y / 2 / zoomscale.k}
+                width={500 / zoomscale.k}
+                height={500 / zoomscale.k}
+                fill="grey"
+                stroke="red"
+                strokeWidth={5}
+              ></rect>
+              {/* ノードを描画 */}
+              {nodedata.map((node, index) => {
+                return (
+                  <ellipse
+                    key={index}
+                    cx={scales.xScale(node.x) / 2}
+                    cy={scales.yScale(node.y) / 2}
+                    rx="5"
+                    ry="5"
+                    fill={node.color}
+                    onClick={() => {
+                      console.log(node.animename);
+                    }}
+                    style={{ cursor: "pointer" }}
+                  ></ellipse>
+                );
+              })}
+            </g>
+          </svg>
+        </div>
       </div>
     </div>
   ) : (
