7f3ad7fb3f7dfb68e66b0c2794079ea47ee625de
H153165g <chrn22018@g.nihon-u.ac.jp>
2025-01-12 22:19:10 +0900

    総合データの作成

---
+++ collect_data/sum.py
@@ -0,0 +1,27 @@
+import json
+
+def get_anime_name(filepath):
+    """アニメ名を JSON ファイルから取得"""
+    with open(filepath, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+months=["01","02","03","04","05","06","07","08","09","10","11","12"]
+
+nodes=get_anime_name("./data/node.json")
+
+data=list()
+for i in range(len(nodes)):
+    viewCount=0
+    commentCount=0
+    likeCount=0
+    videoCount=0
+    for m in range(2006,2026):
+        for n in months:
+            viewCount+=nodes[i]["viewCount"][str(m)][n]
+            commentCount+=nodes[i]["commentCount"][str(m)][n]
+            likeCount+=nodes[i]["likeCount"][str(m)][n]
+            videoCount+=nodes[i]["videoCount"][str(m)][n]
+    data.append({"animename":nodes[i]["animename"],"viewCount":viewCount,"commentCount":commentCount,"likeCount":likeCount,"videoCount":videoCount})
+
+with open("./data/data_All.json", 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
