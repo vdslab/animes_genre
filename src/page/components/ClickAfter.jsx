00273db48a321fa364bf33d9fc4f35385b62b0ac
H153165g <chrn22018@g.nihon-u.ac.jp>
2025-02-05 18:48:06 +0900

    なんだこれ

--- src/page/components/ClickAfter.jsx
+++ src/page/components/ClickAfter.jsx
@@ -22,8 +22,8 @@ const ClickAfter = ({
   // 非同期処理を行う関数
   async function trans(node) {
     if (clickNode != null) {
-      node = node.replace(/<[^>]*>/g, "");  // HTMLタグを削除
-      console.log(node)
+      node = node.replace(/<[^>]*>/g, ""); // HTMLタグを削除
+      console.log(node);
       try {
         const response = await fetch(
           `https://script.google.com/macros/s/AKfycbxwZewBANl5EuM-pnpbTgMwGryNhDapa3aTYCtBSFf5XIOVzgPmSTTxkiw8bj2fChl-AA/exec?text=${node}&source=en&target=ja`
@@ -32,36 +32,31 @@ const ClickAfter = ({
         return feachData.text; // 翻訳されたテキストを返す
       } catch (error) {
         console.error("Error fetching data:", error);
-        return "";  // エラーが発生した場合は空文字を返す
+        return ""; // エラーが発生した場合は空文字を返す
       }
     }
-    return "";  // clickNode が null の場合は空文字を返す
+    return ""; // clickNode が null の場合は空文字を返す
   }
 
-  useEffect(() => {
-    // clickNode が存在する場合にタグを翻訳
-    if (clickNode != null && clickNode.tag.length > 0) {
-      const translateTags = async () => {
-        const translated = await Promise.all(
-          clickNode.tag.map((node) => trans(node.name))
-        );
-        setTranslatedTags(translated); // 翻訳されたタグを状態に保存
-      };
-      translateTags();
-    }
-  }, [clickNode]); // clickNode が変更されるたびに実行される
+  // useEffect(() => {
+  //   // clickNode が存在する場合にタグを翻訳
+  //   if (clickNode != null && clickNode.tag.length > 0) {
+  //     const translateTags = async () => {
+  //       const translated = await Promise.all(
+  //         clickNode.tag.map((node) => trans(node.name))
+  //       );
+  //       setTranslatedTags(translated); // 翻訳されたタグを状態に保存
+  //     };
+  //     translateTags();
+  //   }
+  // }, [clickNode]); // clickNode が変更されるたびに実行される
 
   return (
     <div className="click_After">
-      
-
       {clickNode == null ? (
-      
-        <div >
+        <div>
           <h3>操作説明</h3>
-          <p>
-            このサイトでは、アニメのジャンルと人気度を可視化しました。
-          </p>
+          <p>このサイトでは、アニメのジャンルと人気度を可視化しました。</p>
           <p>
             ノードが近いとジャンルが類似していて、遠いとジャンルは類似してません。
           </p>
@@ -86,7 +81,8 @@ const ClickAfter = ({
           <h3>あらすじ</h3>
           <h4>{clickNode.description != null && clickNode.description.text}</h4>
           <h3>ジャンル</h3>
-          <h4>{translatedTags.join(", ")}</h4> {/* 翻訳されたタグを表示 */}
+          <h4>{clickNode.tag.map((tag) => tag.name + ",")}</h4>{" "}
+          {/* 翻訳されたタグを表示 */}
           <h3>放送期間：</h3>
           <h3>
             {clickNode.startDate.year}年 {clickNode.startDate.month}月{" "}
